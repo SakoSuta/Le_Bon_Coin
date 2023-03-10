fb1d5f2fd9c961aa88c8ad942c00ff847f037cf0
Emilie <EmilieMontpre@outlook.com>
2023-03-10 15:06:34 +0100

    add "title" for db

--- database/migrations/2023_03_10_133829_create_annonces_table.php
+++ database/migrations/2023_03_10_133829_create_annonces_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('annonces', function (Blueprint $table) {
         $table->id();
+        $table->string('title');
         $table->string('email');
         $table->string('name');
         $table->text('description');
