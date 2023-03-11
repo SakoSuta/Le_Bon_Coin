fe070215bae119e46e626302c3ab4b2e23cdc4b5
Emilie <EmilieMontpre@outlook.com>
2023-03-11 15:39:35 +0100

    avancement for the create

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ use App\Http\Controllers\AnnonceController;
 //     return view('welcome');
 // });
 
-Route::get('/', [App\Http\Controllers\AnnonceController::class, 'index'])->name('annonces.index');
+Route::get('/', [App\Http\Controllers\AnnonceController::class, 'index']);
 Route::get('/{id}', [App\Http\Controllers\AnnonceController::class, 'show'])->name('annonces.show');
-Route::get('/create', [App\Http\Controllers\AnnonceController::class, 'create'])->name('annonces.create');
-Route::post('/createDB', [App\Http\Controllers\AnnonceController::class, 'createdOnDB'])->name('annonces.createdOnDB');
-// Route::get('/validate/{token}', [App\Http\Controllers\AnnonceController::class, 'validateAnnonce'])->name('annonces.validate');
-Route::get('/delete/{token}', [App\Http\Controllers\AnnonceController::class, 'delete'])->name('annonces.delete');
+Route::get('/annonces/create', [App\Http\Controllers\AnnonceController::class, 'create'])->name('annonces.create');
+Route::post('/annonces', [App\Http\Controllers\AnnonceController::class, 'createdOnDB'])->name('annonces.createdOnDB');
+Route::get('/annonces/validate/{token}', [App\Http\Controllers\AnnonceController::class, 'validateAnnonce'])->name('annonces.validate');
+Route::get('/annonces/delete/{token}', [App\Http\Controllers\AnnonceController::class, 'delete'])->name('annonces.delete');
