6847af4acd1f0bab8cc7a7c69c54dbad9f76feb1
Emilie <EmilieMontpre@outlook.com>
2023-03-12 12:01:26 +0100

    new file for mail

---
+++ resources/views/emails/checked.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Le Bon Coin</title>
+    </head>
+    <body>
+        <h1>Your annonce has been well verify !</h1>
+        <h2>Also your annonce is now publish in the website ! 👍</h2>
+        <p>But if you don't want it anymore you can delete the annonce by clicking here ⬇️</p>
+        <a href="http://localhost:8000/validate/{{ $token }}">CAREFULL ! this action is irreversible !</a>
+    </body>
+</html>
---
+++ resources/views/emails/delete.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Le Bon Coin</title>
+    </head>
+    <body>
+        <h1>Your annonce has been well deleted !</h1>
+        <p>bye ._.</p>
+    </body>
+</html>
